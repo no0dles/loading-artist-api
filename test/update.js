698335e58182a934a79f8ae0fb397769ac22b09f
Pascal <pascal.bertschi@hsr.ch>
2015-12-12 20:08:33 +0100

    added first test case

--- test/update.js
+++ test/update.js
@@ -1,3 +1,16 @@
-describe('update', function () {
+var hippie = require('hippie');
+var server = require('../server');
 
+describe('update', function () {
+  it('get latest id', function (done) {
+    hippie(server)
+      .json()
+      .get('/update')
+      .expectStatus(200)
+      .end(function(err, res, body) {
+        console.log(body);
+        if (err) throw err;
+        done();
+      });
+  })
 });
